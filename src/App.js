842be62e2482c277dd50174025eb0812c3cd4ee4
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-07-03 00:23:59 +0100

    changed BrowseRouter to HashRouter for gh-pages to work with react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Header />} />
           <Route exact path="/channels" element={<Home />} />
           <Route exact path="/channels/:id" element={<Home />} />
           <Route exact path="/channels/:id/:id" element={<Home />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
